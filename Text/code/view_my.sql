2f206e754cf8d329725cbbecde943557baa70a97
asciishell <asciishell@yandex.ru>
2019-05-26 20:12:37 +0300

    Add end-user views

---
+++ Text/code/view_my.sql
@@ -0,0 +1,48 @@
+CREATE OR REPLACE VIEW my_courses AS
+SELECT c_id, c_name
+FROM course
+WHERE c_visibility
+  AND c_id IN (SELECT p_course FROM participation WHERE p_client = userid());
+COMMENT ON VIEW my_courses IS 'Список курсов, доступных пользователю';
+
+CREATE OR REPLACE VIEW my_blocks AS
+SELECT c_id, c_name, b_id, b_theme
+FROM block
+         INNER JOIN my_courses on block.b_course = c_id
+WHERE block.b_visibility;
+COMMENT ON VIEW my_blocks IS 'Список блоков, доступных пользователю';
+
+CREATE OR REPLACE VIEW my_lectures AS
+SELECT c_id,
+       c_name,
+       b_id,
+       b_theme,
+       l_id,
+       l_name,
+       l_content,
+       make_interval(secs := char_length(l_content) / 25.0) as l_duration
+FROM lecture
+         INNER JOIN my_blocks ON l_block = b_id
+WHERE l_visibility;
+COMMENT ON VIEW my_lectures IS 'Список лекций, доступных пользователю';
+
+CREATE OR REPLACE VIEW my_tests AS
+SELECT c_id,
+       c_name,
+       b_id,
+       b_theme,
+       t_id,
+       t_name,
+       t_task,
+       t_max,
+       t_duration
+FROM testmaterial
+         INNER JOIN my_blocks ON t_block = b_id
+WHERE t_visibility;
+COMMENT ON VIEW my_tests IS 'Список тестов, доступных пользователю';
+
+CREATE OR REPLACE VIEW my_attempts AS
+SELECT *
+FROM attempt
+WHERE a_author = userid();
+COMMENT ON VIEW my_attempts IS 'Список попыток пользователя';
