36999b850331a3c84f425e0f4a1632328779b6ad
asciishell <asciishell@yandex.ru>
2019-05-31 00:12:37 +0300

    Revert "Update structure "
    This reverts commit fbef273985349d73a443959f0b7b96ea5a1cad3e.

--- Text/code/tab_block.sql
+++ Text/code/tab_block.sql
@@ -3,34 +3,5 @@ CREATE TABLE Block
     B_ID         SERIAL PRIMARY KEY,
     B_THEME      VARCHAR(100) NOT NULL,
     B_VISIBILITY BOOLEAN      NOT NULL DEFAULT FALSE,
-    B_COURSE     INTEGER      NOT NULL REFERENCES Course (C_ID),
-    B_SORTORDER  INTEGER      NOT NULL
+    B_COURSE     INTEGER      NOT NULL REFERENCES Course (C_ID)
 );
--------------------------------------------------------------------------
--- Процедура для автоматического добавления нового блока
--- в конец текущего курса.
--- Параметр sortorder подбирается автоматически на основе последнего
--- блока текущего курса.
-CREATE OR REPLACE PROCEDURE
-    create_block(theme VARCHAR(100),
-                 visibility BOOLEAN,
-                 course_id INTEGER)
-AS
-$$
-DECLARE
-    last_id INTEGER;
-BEGIN
-    SELECT MAX(B_SORTORDER) INTO last_id
-    FROM Block
-    WHERE B_COURSE = course_id;
-
-    IF last_id IS NULL THEN
-        last_id := 1000;
-    ELSE
-        last_id := (div(last_id, 1000) + 1) * 1000;
-    end if;
-    INSERT INTO Block
-    VALUES (DEFAULT, theme, visibility, course_id, last_id);
-END
-$$
-    LANGUAGE PLPGSQL;
--- Text/code/tab_testMaterial.sql
+++ Text/code/tab_testMaterial.sql
@@ -6,40 +6,5 @@ CREATE TABLE TestMaterial
     T_MAX        INTEGER      NOT NULL CHECK ( T_MAX >= 0 ),
     T_DURATION   INTERVAL CHECK ( T_DURATION > '0'::INTERVAL ),
     T_VISIBILITY BOOLEAN      NOT NULL DEFAULT FALSE,
-    T_BLOCK      INTEGER      NOT NULL REFERENCES Block (B_ID),
-    T_SORTORDER  INTEGER      NOT NULL
+    T_BLOCK      INTEGER      NOT NULL REFERENCES Block (B_ID)
 );
--------------------------------------------------------------------------
--- Процедура для автоматического добавления новой лекции
--- в конец текущего блока.
--- Параметр sortorder подбирается автоматически на основе последнего
--- задания текущего блока.
-CREATE OR REPLACE PROCEDURE
-    create_testmaterial(name VARCHAR(100),
-                        task TEXT,
-                        max_score INTEGER,
-                        duration INTERVAL,
-                        visibility BOOLEAN,
-                        block_id INTEGER)
-AS
-$$
-DECLARE
-    last_id INTEGER;
-BEGIN
-    SELECT GREATEST((SELECT MAX(T_SORTORDER)
-                     FROM testmaterial
-                     WHERE T_BLOCK = block_id),
-                    (SELECT MAX(L_SORTORDER)
-                     FROM Lecture
-                     WHERE L_BLOCK = block_id)) INTO last_id;
-
-    IF last_id IS NULL THEN
-        last_id := 1000;
-    ELSE
-        last_id := (div(last_id, 1000) + 1) * 1000;
-    end if;
-    INSERT INTO TestMaterial
-    VALUES (DEFAULT, name, task, max_score, duration, visibility, block_id, last_id);
-END
-$$
-    LANGUAGE PLPGSQL;
