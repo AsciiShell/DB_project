fbef273985349d73a443959f0b7b96ea5a1cad3e
asciishell <asciishell@yandex.ru>
2019-05-31 00:12:37 +0300

    Update structure

--- Text/code/tab_lecture.sql
+++ Text/code/tab_lecture.sql
@@ -4,5 +4,38 @@ CREATE TABLE Lecture
     L_NAME       VARCHAR(100) NOT NULL,
     L_CONTENT    TEXT,
     L_VISIBILITY BOOLEAN      NOT NULL DEFAULT FALSE,
-    L_BLOCK      INTEGER      NOT NULL REFERENCES Block (B_ID)
+    L_BLOCK      INTEGER      NOT NULL REFERENCES Block (B_ID),
+    L_SORTORDER  INTEGER      NOT NULL
 );
+-------------------------------------------------------------------------
+-- Процедура для автоматического добавления новой лекции
+-- в конец текущего блока.
+-- Параметр sortorder подбирается автоматически на основе последнего
+-- задания текущего блока.
+CREATE OR REPLACE PROCEDURE
+    create_lecture(name VARCHAR(100),
+                   content TEXT,
+                   visibility BOOLEAN,
+                   block_id INTEGER)
+AS
+$$
+DECLARE
+    last_id INTEGER;
+BEGIN
+    SELECT GREATEST((SELECT MAX(T_SORTORDER)
+                     FROM testmaterial
+                     WHERE T_BLOCK = block_id),
+                    (SELECT MAX(L_SORTORDER)
+                     FROM Lecture
+                     WHERE L_BLOCK = block_id)) INTO last_id;
+
+    IF last_id IS NULL THEN
+        last_id := 1000;
+    ELSE
+        last_id := (div(last_id, 1000) + 1) * 1000;
+    end if;
+    INSERT INTO Lecture
+    VALUES (DEFAULT, name, content, visibility, block_id, last_id);
+END
+$$
+    LANGUAGE PLPGSQL;
